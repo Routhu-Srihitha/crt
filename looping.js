a3cf57f0247bf988371d250dd46c6008eef07950
Routhu-Srihitha <srhitharouthu08@gmail.com>
2025-01-08 18:59:42 +0530

    new file added

---
+++ looping.js
@@ -0,0 +1,49 @@
+//for(let i=1; i<=10;i++){
+    //console.log("Javascript");
+//}
+
+//for(let i=1;i<=100;i++){
+    //console.log(i);
+//}
+
+//for (let i = 1; i <= 100; i++) {
+    //if (i % 2 === 0) {
+      //  console.log(i + " is even");
+   // } else {
+       // console.log(i + " is odd");
+    //}
+//}
+
+
+//let factorial = 1;
+//for (let i = 1; i <= 5; i++) {
+   // factorial *= i;
+//}
+//console.log("Factorial of 5 is: " + factorial);
+
+//let mul=1;
+//let num = Number(prompt("enter a number"));
+//for(let i=1;i<=num;i++){
+    //console.log(i);
+  //  mul = mul*i;
+//}
+//console.log(mul);
+
+//let i =1;
+//while(i<=5){
+    //console.log("Hello Wake up and please interact with me");
+  //  i++;
+//}
+
+//let i=1;
+//do 
+//{
+   // i++;
+    //console.log ("Hello JavaScript");
+//}while(i>=5);
+
+function addToCart(){
+    alert("product is added to cart sucessfully");
+    document.body.style.background="red";
+}
+addToCart();
