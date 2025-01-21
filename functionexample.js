acf69e9f6c956874e2a3e7c8a7a7bfdc9bda7748
Routhu-Srihitha <srhitharouthu08@gmail.com>
2025-01-21 10:51:19 +0530

    adding new files

---
+++ functionexample.js
@@ -0,0 +1,72 @@
+//function addToCart(){
+  //  alert("product is added to cart sucessfully");
+//}
+//addToCart();
+
+
+//function demo(){
+ //   console.log("function  is executing.....");
+//}
+//demo();
+//demo();
+
+//5,6
+//8,9
+//3,4
+//function add(a,b){
+   // console.log(a,b);
+    //console.log(a+b);
+//}
+//add(5,6);
+//add(8,9);
+//add(3,4);
+
+//function test(){
+   // console.log("named function is executing...");
+//}
+//test();
+//let a =10;
+//let x = function(){
+    //console.log("ananymus is not executing but executing with variables");
+//}
+//console.log(x);
+//x();
+
+//(
+    //function(){
+    //    console.log("IIFE");
+  //  }
+//)();
+
+//function demo(){
+
+//}
+//demo();
+
+//let x =_=>{console.log("Aroow Function")};
+//x();
+
+//function demo(a) {
+  //  console.log("hello");
+//}
+//demo();
+
+//let x = ()=>{console.log("Arrow Function")};
+//x();
+
+//let x = a => console.log(a);
+//x(4);
+
+//function test1(a,b){
+//console.log("Hello i am priniting before return statement");
+  //  return(a+b);
+  //console.log("Hello i am printing after return keyword")
+//};
+//console.log(test1(5,5));
+
+//let x = (a,b) => {return a*b};
+//console.log(x(5,5));
+
+//let y = (a,b) => a+b;
+//console.log(y(100,200));
+
