742269ce5727b98f8b49871405270e4c7fede6c4
Routhu-Srihitha <srhitharouthu08@gmail.com>
2025-01-23 11:41:54 +0530

    Your descriptive commit message

--- async.js
+++ async.js
@@ -38,28 +38,61 @@
 
 // ! API fetching
 
-function fetchUsers(){
-   let response = fetch("https://jsonplaceholder.typicode.com/users");
+//function fetchUsers(){
+  // let response = fetch("https://jsonplaceholder.typicode.com/users");
    // console.log(response);
-   response.then(res=>{
+   //response.then(res=>{
        // console.log(res)
        // console.log(res.json());
-       return res.json().then(data=>{
+     //  return res.json().then(data=>{
            // console.log(data);
-           let store = document.getElementById("store");
-           data.map(user=>{
-               console.log(user);
-               store.innerHTML +=`
-               <tr>
-               <td>${user.id}</td>
-               <td>${user.name}</td>
-               <td>${user.email}</td>
-               <td>${user.company.name}</td>
-               </tr>
-               `
-           })
-       })
-   })
-   .catch(err=>console.log(err))
+       //    let store = document.getElementById("store");
+         //  data.map(user=>{
+           //    console.log(user);
+             //  store.innerHTML +=`
+               //<tr>
+               //<td>${user.id}</td>
+               //<td>${user.name}</td>
+               //<td>${user.email}</td>
+               //<td>${user.company.name}</td>
+               //</tr>
+               
+           //})
+      // })
+  // })
+  // .catch(err=>console.log(err))
+//}
+//fetchUsers();
+
+//function demo()
+//{
+  //console.log("Start");
+  //console.log(10);
+  //console.log(20);
+  //return;
+  //console.log(30);
+  //console.log("End");
+//}
+//demo();
+
+//let p = new Promise((resolve , reject)=>{
+  //setTimeout(()=>{
+    //resolve("success")
+  //},4000);
+//});
+
+ //async function demo(){
+  //console.log("start");
+  //let x = await p;
+  //console.log(x);
+  //console.log("end");
+//}
+
+//demo();
+
+async function fetchUsers(){
+  let response = fetch("https://api.github.com/users");
+  let data =(await response).json();
+  console.log(data);
 }
-fetchUsers();
+fetchUsers()
