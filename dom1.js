6cef325fd2ae47c1e8476c5ed00b5cc79cbe26bb
Routhu-Srihitha <srhitharouthu08@gmail.com>
2025-01-29 10:47:55 +0530

    Your descriptive commit message

--- dom1.js
+++ dom1.js
@@ -15,59 +15,109 @@
 // document.body.appendChild(image)
 
 
-//let form = document.querySelector("form");
-//let username = document.querySelector("#uName");
-//let email = document.querySelector("#uMail");
-//let psw = document.querySelector("#uPsw");
+// let form = document.querySelector("form");
+// let username = document.querySelector("#uName");
+// let email = document.querySelector("#uMail");
+// let psw = document.querySelector("#uPsw");
 
-//form.addEventListener("submit",(event)=>{
-    //event.preventDefault();
-    // console.log(event);
-    // console.log("form submitted");
-    // console.log(username.value);
-   // let name =username.value;
-    //let mail = email.value;
-    //let password = psw.value;
-    //let userDetails={
-      //  name:name,
-        //mail : mail,
-        //password:password
-    //} ;
-    // console.log(userDetails);
-    //localStorage.setItem("userData" , JSON.stringify(userDetails))
-//})>
+// form.addEventListener("submit",(event)=>{
+//     event.preventDefault();
+//     // console.log(event);
+//     // console.log("form submitted");
+//     // console.log(username.value);
+//     let name =username.value;
+//     let mail = email.value;
+//     let password = psw.value;
+//     let userDetails={
+//         name:name,
+//         mail : mail,
+//         password:password
+//     } ;
+//     // console.log(userDetails);
+//     localStorage.setItem("userData" , JSON.stringify(userDetails))
+// })
 
 
 // localStorage.clear()
 
-let mainEle = document.createElement("div");
-mainEle.setAttribute("id" , "mainblock");
 
-console.log(mainEle);
+// ! 
 
-let topEle = document.createElement("div");
-topEle.setAttribute("class" , "topBlock");
-//consoel.log(topEle);
+// let mainEle = document.createElement("div");
+// mainEle.setAttribute("id" , "mainBlock");
 
-let image = document.createElement("img");
-image.src="c:\Users\srihi\Downloads\british-shorthair-7880908_1280.jpg"
-image.width="300";
-image.height="300";
-image.style.objects="cover"
+// console.log(mainEle);
 
-let bottomEle = document.createElement("div");
-buttomEle.setAttribute("class" , "bottomElement");
-//console.log(bottomEle);
-let h1 = document.createElement("h1");
-h1.innerTexts="CAT";
 
-let btn = document.createElement("button");
-btn.innerText="View More";
+// let topEle = document.createElement("div");
+// topEle.setAttribute("class" , "topBlock");
+// // console.log(topEle);
 
-bottomEle.appendChild(h1);
-bottomEle.appendChild(btn);
-topEle.appendChild(image);
-mainEle.appendChild(topEle);
-mainEle.appendChild(bottomEle);
+// let image = document.createElement("img");
+// image.src="https://cdn.pixabay.com/photo/2023/02/09/16/43/cat-7779237_640.jpg";
+// image.width="300";
+// image.height="300";
+// image.style.objectFit="cover"
 
-document.body.appendChild(mainEle)
+// let bottomEle = document.createElement("div");
+// bottomEle.setAttribute("class" , "bottomBlock");
+// // console.log(bottomEle);
+// let h1 = document.createElement("h1");
+// h1.innerText="CAT";
+
+// let btn = document.createElement("button");
+// btn.innerText="View More";
+
+// bottomEle.appendChild(h1);
+// bottomEle.appendChild(btn);
+// topEle.appendChild(image);
+// mainEle.appendChild(topEle);
+// mainEle.appendChild(bottomEle);
+
+// document.body.appendChild(mainEle)
+
+
+// !
+let form = document.querySelector("form");
+let username = document.getElementById("uName");
+let password = document.getElementById("uPass");
+let check = document.getElementById("check");
+let show = document.getElementById("show");
+
+let gender = document.getElementsByName("gender");
+// console.log(gender);
+
+// ! hiding and showing the password
+check.addEventListener("click" , (event)=>{
+    // console.log(event.target.checked);
+    if(event.target.checked == true){
+        password.setAttribute("type" , "text");
+        show.innerText = "hide password";
+    }else{
+        password.setAttribute("type" , "password");
+        show.innerText="show password";
+    }
+})
+
+form.addEventListener("submit" , event=>{
+    event.preventDefault();
+    let un = username.value;
+    let up = password.value;
+    let gen = "";
+    for(let i = 0;i<=gender.length-1;i++){
+        // console.log(gender[i].value);
+        // console.log(gender[i].checked);
+        if(gender[i].checked == true){
+            // console.log(gender[i].value);
+            gen += gender[i].value;
+        }
+    };
+    let userDetails={
+        username:un,
+        password:up,
+        gender:gen
+
+    };
+    console.log(userDetails);
+    sessionStorage.setItem("userData" ,JSON.stringify(userDetails) )
+})
